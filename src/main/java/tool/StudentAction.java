75a9255815d0f0e3faba19b3c3766978b1bc6796
kotoka <fko2547055@stu.o-hara.ac.jp>
2026-04-21 15:03:27 +0900

    学生一覧クラスの途中

---
+++ src/main/java/tool/StudentAction.java
@@ -0,0 +1,14 @@
+package tool;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class StudentAction extends Action {
+
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
