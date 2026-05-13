dc763501e24bf146ba0387a1a3538efe0708687d
ooi2572063 <ooi2572063@stu.o-hara.ac.jp>
2026-05-13 15:04:22 +0900

    途中

---
+++ src/main/java/dao/TestDao.java
@@ -0,0 +1,51 @@
+package dao;
+
+import bean.School;
+import bean.Student;
+import bean.Subject;
+import scoremanager.main.Test;
+
+public class TestDao extends Dao {
+	
+	private string baseSql;
+	
+	public Test get(
+			Student student,
+			Subject subject,
+			School school,
+			int no
+			) {
+			return null;
+			}
+	
+	public List<Test> postFilter(
+			ResultSet rs,
+			School school
+			){
+		return null;
+			}
+	public List<Test> filter(
+			int entYear,
+			String classNum,
+			Subject subject,
+			int num,
+			School school
+			){
+		return null
+			}
+	public boolean save(List<Test> list) {
+	       return false;
+	    }
+	public boolen save(
+			Test test,
+			Connectionn connection
+			) {
+				
+			return false;
+	}
+	
+	
+
+}
+
+
