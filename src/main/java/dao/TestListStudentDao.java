bb406ec68b3dd26e523feb8d22dcab3fc2df6978
ooi2572063 <ooi2572063@stu.o-hara.ac.jp>
2026-05-19 11:52:07 +0900

    ok

---
+++ src/main/java/dao/TestListStudentDao.java
@@ -0,0 +1,24 @@
+package dao;
+
+import java.sql.ResultSet;
+import java.util.List;
+
+import bean.Student;
+import bean.TestListStudent;
+
+public class TestListStudentDao extends Dao {
+	private String baseSql;
+	
+	public List<TestListStudent> postFIlter(
+			ResultSet rSet
+			){
+		return null;
+	}
+	public List<TestListStudent> filter(
+			Student student
+			){
+		return null;
+	}
+	}
+	
+
