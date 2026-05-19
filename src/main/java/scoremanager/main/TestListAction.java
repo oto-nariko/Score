bfb116f2c944cb82cf2d692ebbce6c435ea9eb2f
ooi2572063 <ooi2572063@stu.o-hara.ac.jp>
2026-05-19 15:09:32 +0900

    途中

--- src/main/java/scoremanager/main/TestListAction.java
+++ src/main/java/scoremanager/main/TestListAction.java
@@ -3,14 +3,36 @@ package scoremanager.main;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import bean.School;
+import bean.Teacher;
+import dao.ClassNumDao;
+import dao.SubjectDao;
 import tool.Action;
 
 public class TestListAction extends Action {
 
 	@Override
-	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
-		// TODO 自動生成されたメソッド・スタブ
+	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {  
+		Teacher teacher=(Teacher) req.getSession().getAttribute("user");
+		School school=teacher.getschool();
+		
+		String entYear=req.getParameter("f1");
+		String classNum=req.getParameter("f2");
+		String subjectCd=req.getParameter("f3");
+		String count=req.getParameter("f4");
+		
+		int entYear = 0;
+		if (entYearStr != null && !entYearStr.isEmpty()) {
+			entYear = Integer.parseInt(entYearStr);
+		}
+		
+		ClassNumDao classNumDao=new ClassNumDao();
+		SubjectDao subjectDao=new SubjectDao();
+		
+		
+        
 
+		
 	}
 
 }
