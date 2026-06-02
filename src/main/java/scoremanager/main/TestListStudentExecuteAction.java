4182f6eed15e6a79f01edb00a86ced975cf62436
ooi2572063 <ooi2572063@stu.o-hara.ac.jp>
2026-06-02 10:26:58 +0900

    ok

--- src/main/java/scoremanager/main/TestListStudentExecuteAction.java
+++ src/main/java/scoremanager/main/TestListStudentExecuteAction.java
@@ -21,36 +21,36 @@ public class TestListStudentExecuteAction extends Action {
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
 
-		HttpSession session = req.getSession();
+		HttpSession session=req.getSession();
 		if (session == null) {
 			res.sendRedirect(req.getContextPath() + "/scoremanager/Login.action");
 			return;
 		}
 
-		Teacher teacher = (Teacher) session.getAttribute("user");
+		Teacher teacher=(Teacher) session.getAttribute("user");
 		if (teacher == null) {
 			res.sendRedirect(req.getContextPath() + "/scoremanager/Login.action");
 			return;
 			}
 
-		String studentNo = req.getParameter("f4");
+		String studentNo=req.getParameter("f4");
 
-		ClassNumDao cDao = new ClassNumDao();
-		SubjectDao subDao = new SubjectDao();
-		TestListStudentDao tlsDao = new TestListStudentDao();
+		ClassNumDao cDao=new ClassNumDao();
+		SubjectDao subDao=new SubjectDao();
+		TestListStudentDao tlsDao=new TestListStudentDao();
 
-		List<String> classList = cDao.filter(teacher.getSchool());
-		List<Subject> subjectList = subDao.filter(teacher.getSchool());
-		List<TestListStudent> tests = new ArrayList<>();
+		List<String> classList=cDao.filter(teacher.getSchool());
+		List<Subject> subjectList=subDao.filter(teacher.getSchool());
+		List<TestListStudent> tests=new ArrayList<>();
 
 		if (studentNo == null || studentNo.isEmpty()) {
 			req.setAttribute("error", "このフィールドを入力してください。");
 			} else {
-				Student student = new Student();
+				Student student=new Student();
 				student.setNo(studentNo);
 				student.setSchool(teacher.getSchool());
 
-				tests = tlsDao.filter(student);
+				tests= tlsDao.filter(student);
 				if (tests == null || tests.size() == 0) {
 					req.setAttribute("error", "学生情報が存在しませんでした");
 					}
