221f7577aa1700b470ab70e2fd0234ecc9340fbc
airi-de <fko2547054@stu.o-hara.ac.jp>
2026-06-09 15:07:50 +0900

    コメント

--- src/main/java/scoremanager/main/SubjectDeleteExecuteAction.java
+++ src/main/java/scoremanager/main/SubjectDeleteExecuteAction.java
@@ -17,22 +17,25 @@ public class SubjectDeleteExecuteAction extends Action {
 		// TODO 自動生成されたメソッド・スタブ
 		HttpSession session =req.getSession();
 		SubjectDao sDao=new SubjectDao();
-		
+		//ログインチェック
 		Teacher teacher = (Teacher) session.getAttribute("user");
 		//ログインしていない場合はログイン画面に飛ばす
 		if (teacher == null) {
 			res.sendRedirect(req.getContextPath() + "/scoremanager/Login.action");
 			return;
 		}
+		
+		//削除画面から送信されてきた削除対象の科目コードを取得
 		String cd=req.getParameter("subject_cd");
+		//ログイン中のティーチャーが所属する学校情報を取得
 		School school=teacher.getSchool();
-		
+		//削除用のSubject型のBeanオブジェクトを作成して特定に必要なデータをセット
 		Subject subject=new Subject();
-		subject.setCd(cd);
-		subject.setSchool(school);
-		
+		subject.setCd(cd);			//消したい科目コードセット
+		subject.setSchool(school);	//学校情報せっと
+		//データベースから該当する科目のレコードを削除する
 		sDao.delete(subject);
-		
+		//削除完了画面へ画面遷移
 		req.getRequestDispatcher("/subject_delete_done.jsp").forward(req, res);
 		
 	}
